a4189cbbef8f09ca6e4db261acb2c54cae88fe4a
aidancrowther <aidan.crowther@outlook.com>
2018-10-10 11:45:15 -0400

    Branched off of brittny

--- TileRummy/src/main/java/COMP3004/Strategy1.java
+++ TileRummy/src/main/java/COMP3004/Strategy1.java
@@ -13,7 +13,41 @@
 
 package COMP3004;
 
+import java.util.ArrayList;
+
 public class Strategy1 extends ArtificialIntelligence
 {
+    public Strategy1(){
 
+    }
+
+    public void drawTile(){
+        
+    }
+
+    public void selectTile(Meld inMeld, Meld outMeld, Tile tile){
+
+    }
+
+    /*
+     * Terminal View: Loops through the prompts until player enters text to complete their turn, then returns this.table.
+     * GUI View: Allows the user to interact with the table until the user presses complete turn button. When button is pressed end the user’s turn, and the method returns this.table.
+     * AI: Strategies
+     */
+    public Table play(){
+        return null;
+    }
+
+    /*
+     * The view classes observe the model. This function will update their local table to the game board after every turn. 
+     * This should set the table to match the table in scrummy, therefore we do not need setTable
+     */
+    public void update(Table table){
+
+    }
+
+    //The hand needs a setter since there are more than one hand in the game, making it unsuitable to observe
+    public void setHand(ArrayList<Tile> hand){
+
+    }
 }
