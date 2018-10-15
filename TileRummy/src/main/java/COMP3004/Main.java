c992f044d9d919dda5e8db7ad4ed1a1135bbb81b
Brittny Lapierre <brittny.lapierre@gmail.com>
2018-10-15 00:18:17 -0400

    set view type in main

--- TileRummy/src/main/java/COMP3004/Main.java
+++ TileRummy/src/main/java/COMP3004/Main.java
@@ -3,6 +3,7 @@ package COMP3004;
 public class Main {
     public static void main (String[] args) {
         Controller controller = new Controller();
+        controller.setViewType("t"); // TODO: make interactive
         controller.run();
     }
 }
