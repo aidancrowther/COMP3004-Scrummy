d360462d74c066d22676b4af3fa09a1c7d2e705a
Brittny Lapierre <brittny.lapierre@gmail.com>
2018-10-22 08:23:12 -0400

    Changing to terminal for merge

--- TileRummy/src/main/java/COMP3004/Main.java
+++ TileRummy/src/main/java/COMP3004/Main.java
@@ -5,7 +5,7 @@ import COMP3004.controllers.Controller;
 public class Main {
     public static void main (String[] args) {
         Controller controller = new Controller();
-        String viewType = "g";
+        String viewType = "t";//"g";
         controller.setViewType(viewType); // TODO: make interactive
         controller.run();
     }
