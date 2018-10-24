a67da829d62ee3aebb8340e32d406621ea3e4f35
A <ellisjglennie@gmail.com>
2018-10-23 20:10:30 -0400

    main: added Strategy2 and Strategy3 empty classes

---
+++ TileRummy/src/main/java/COMP3004/Strategy2.java
@@ -0,0 +1,60 @@
+/* Carleton University
+ * Fall 2018
+ * 
+ * COMP 3004
+ * JP Coriveau
+ * 
+ * Group 6
+ * David N. Zilio
+ * 
+ * AI Strategy2 should do exactly as the spec requires
+ * -- plays its initial 30 points only if another player has already played tiles on the table.
+ * -- if it can play all its tiles, it does.	
+ * -- otherwise, plays only the tiles of its hand that require using tiles on the table to make melds.
+ */
+
+package COMP3004;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Strategy2 extends ArtificialIntelligence
+{
+
+
+    public Strategy2(){
+
+    }
+
+
+    /*
+     * Terminal View: Loops through the prompts until player enters text to complete their turn, then returns this.table.
+     * GUI View: Allows the user to interact with the table until the user presses complete turn button. When button is pressed end the user’s turn, and the method returns this.table.
+     * AI: Strategies 1 - 4
+     */
+    public Table play(){
+
+        /*  
+
+        * -- plays its initial 30 points only if another player has already played tiles on the table.
+        * -- if it can play all its tiles, it does.	
+        * -- otherwise, plays only the tiles of its hand that require using tiles on the table to make melds.
+
+        */
+
+        return null;
+    }
+
+
+
+
+    
+    
+
+
+
+
+
+
+}
---
+++ TileRummy/src/main/java/COMP3004/Strategy3.java
@@ -0,0 +1,64 @@
+/* Carleton University
+ * Fall 2018
+ * 
+ * COMP 3004
+ * JP Coriveau
+ * 
+ * Group 6
+ * David N. Zilio
+ * 
+ * AI Strategy3 should do exactly as the spec requires
+ * -- plays initial 30 as soon as it can.
+ * -- If it can play all its tiles, it does.	
+ * -- Else, each turn
+ *      -> If no other player has 3 fewer tiles than p3, then act like Strategy2
+ *      -> Else, act like Strategy1
+ */
+
+package COMP3004;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Strategy3 extends ArtificialIntelligence
+{
+
+
+    public Strategy3(){
+
+    }
+
+
+    /*
+     * Terminal View: Loops through the prompts until player enters text to complete their turn, then returns this.table.
+     * GUI View: Allows the user to interact with the table until the user presses complete turn button. When button is pressed end the user’s turn, and the method returns this.table.
+     * AI: Strategies 1 - 4
+     */
+    public Table play(){
+
+        /*
+
+        * -- plays initial 30 as soon as it can.
+        * -- If it can play all its tiles, it does.	
+        * -- Else, each turn
+        *      -> If no other player has 3 fewer tiles than p3, then act like Strategy2
+        *      -> Else, act like Strategy1
+
+        */
+
+        return null;
+    }
+
+
+
+
+    
+    
+
+
+
+
+
+
+}
