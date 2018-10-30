5a3729b3ef095ede9cf7ebd1084cdcd14d12b169
Brittny Lapierre <brittny.lapierre@gmail.com>
2018-10-29 22:59:35 -0400

    Scrummy validate table test

--- TileRummy/src/test/java/COMP3004/ScrummyTest.java
+++ TileRummy/src/test/java/COMP3004/ScrummyTest.java
@@ -1,7 +1,11 @@
 package COMP3004;
 
+import COMP3004.controllers.GameInteractionController;
 import COMP3004.controllers.TerminalViewController;
+import COMP3004.models.Meld;
 import COMP3004.models.Scrummy;
+import COMP3004.models.Table;
+import COMP3004.models.Tile;
 import COMP3004.oberver_pattern.TableObserver;
 import org.junit.Test;
 
@@ -56,5 +60,40 @@ public class ScrummyTest
         for(TableObserver observer : s.getObservers()){
             assertEquals(observer.getTable(), s.getTable());
         }
+    }
+
+    @Test
+    public void testValidateTable(){
+        Scrummy scrummy = new Scrummy();
+        GameInteractionController gameInteractionController = new GameInteractionController();
+        scrummy.registerObserver(gameInteractionController);
+        
+        Meld bad = new Meld();
+        bad.add(new Tile('R', 1));
+        bad.add(new Tile('R', 1));
+
+        Table inValidTable = new Table();
+        inValidTable.add(bad);
+
+        assertFalse(table.isValid());
+
+        scrummy.validatePlayerMove(inValidTable);
+
+        assertNotEquals(scrummy.getTable(), inValidTable);
+        assertNotEquals(gameInteractionController.getTable(), inValidTable);
+
+        Meld good = new Meld();
+        good.add(new Tile('R', 1));
+        good.add(new Tile('B', 1));
+        good.add(new Tile('G', 1));
+
+
+        Table validTable = new Table();
+        validTable.add(good);
+
+        scrummy.validatePlayerMove(validTable);
+
+        assertEquals(scrummy.getTable(), validTable);
+        assertEquals(gameInteractionController.getTable(), validTable);
     }
 }
