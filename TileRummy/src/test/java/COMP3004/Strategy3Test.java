803e2cd9e7aeb942548d4a9bb16c1629a21a7fce
A <ellisjglennie@gmail.com>
2018-11-03 16:14:33 -0400

    AI: p3 work

--- TileRummy/src/test/java/COMP3004/Strategy3Test.java
+++ TileRummy/src/test/java/COMP3004/Strategy3Test.java
@@ -5,9 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Collections;
 import java.util.Iterator;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeNoException;
 
 import COMP3004.artificial_intelligence.*;
 import COMP3004.models.Meld;
@@ -19,7 +17,6 @@ import org.junit.Test;
 public class Strategy3Test {
 
     /*
-    - Test breaking 30
     - Test the observer
     - Test playing when observer calls Strategy 1 (another player has 3 fewer tiles)
     - Test playing when observer calls Strategy 2 (no other players have 3 fewer tiles)
