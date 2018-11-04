f9bb1c35e0222bbc1856588c9ff59a5c714d5c32
A <ellisjglennie@gmail.com>
2018-11-04 15:46:57 -0500

    Strategy3Test: more tests

--- TileRummy/src/test/java/COMP3004/Strategy3Test.java
+++ TileRummy/src/test/java/COMP3004/Strategy3Test.java
@@ -9,10 +9,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeNoException;
 
 import COMP3004.artificial_intelligence.*;
-import COMP3004.models.Scrummy;
-import COMP3004.models.Meld;
-import COMP3004.models.Table;
-import COMP3004.models.Tile;
+import COMP3004.controllers.Controller;
+import COMP3004.models.*;
 import org.junit.Test;
 
 
@@ -20,16 +18,13 @@ public class Strategy3Test {
 
 
 
-    public void testBreak30() {
+    public void testCantBreak30() {
         //Ensure play() does not change the table if it cannot break 30
-        //and does break 30 if it can
 
         //Generate tiles to use
         Tile tile1 = new Tile('O', 9);
         Tile tile2 = new Tile('O', 8);
         Tile tile3 = new Tile('O', 7);
-        Tile tile4 = new Tile('O', 10);
-
         Meld m1 = new Meld();
         m1.add(new Tile('R', 1));
         m1.add(new Tile('R', 2));
@@ -54,10 +49,36 @@ public class Strategy3Test {
         //Assert that the player does not play onto the table
         Table output1 = AI3.play(AI3.getHand());
         assertTrue(output1.getMelds().size() == 2);
+    }
 
-        //Assert that the player does play onto the table when 30 is available
+    @Test
+    public void testCanBreak30() {
+        //Generate tiles to use
+        Tile tile1 = new Tile('O', 9);
+        Tile tile2 = new Tile('O', 8);
+        Tile tile3 = new Tile('O', 7);
+        Tile tile4 = new Tile('O', 10);
+        Meld m1 = new Meld();
+        m1.add(new Tile('R', 1));
+        m1.add(new Tile('R', 2));
+        m1.add(new Tile('R', 3));
+
+        //Initialize a hand that can generate a play of 30
+        Meld hand = new Meld();
+        hand.add(tile1);
+        hand.add(tile2);
+        hand.add(tile3);
         hand.add(tile4);
+
+        //Initialize AI with the specified hand
+        Strategy3 AI3 = new Strategy3();
         AI3.setHand(hand);
+        AI3.setScore(0);
+
+        //Build a table
+        Table table = new Table();
+        table.add(m1);
+        AI3.setTable(table);
 
         Table output2 = AI3.play(AI3.getHand());
         assertTrue(output2.getMelds().size() == 3);
@@ -66,7 +87,9 @@ public class Strategy3Test {
 
     @Test
     public void testObserver() {
-        Scrummy scrummy = new Scrummy(); 
+        Controller controller = new Controller();
+        
+
         
 
 
