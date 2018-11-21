73b7c03180dfa060cbb81b1cd06f6f7f3bace87c
A <ellisjglennie@gmail.com>
2018-11-20 21:54:07 -0500

    Joker: Created skeleton Joker class JokerTest: wrote testJokerChanging()

---
+++ TileRummy/src/main/java/COMP3004/models/Joker.java
@@ -0,0 +1,14 @@
+package COMP3004.models;
+
+import COMP3004.models.Tile;
+
+public class Joker extends Tile {
+
+
+    public Joker() {
+        super('J', 0);
+    }
+
+
+
+}
--- TileRummy/src/test/java/COMP3004/JokerTest.java
+++ TileRummy/src/test/java/COMP3004/JokerTest.java
@@ -4,24 +4,50 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import COMP3004.models.Tile;
+import COMP3004.models.Joker;
 import COMP3004.models.Meld;
 import org.junit.Test;
 
 
 public class JokerTest {
 
-    //assert that Tile.isJoker() can identify which cards are jokers and which are not
 
-    //assert that a joker can be created and assigned different values
-    
-    //assert that a joker can be used in a meld
+    //assert that a joker can be created and assigned different values, acting as a true wild card
+    @Test
+    public void testJokerChanging() {
+        Joker j = new Joker();
+
+        assertTrue(j.getColour() == 'J');
+        assertTrue(j.getValue() == 0);
+
+        j.setColour('B');
+        j.setValue(12);
+
+        assertTrue(j.getColour() == 'B');
+        assertTrue(j.getValue() == 12);
+    }
+
+
+    //assert that isJoker() can identify which cards are jokers and which are not
+    @Test
+    public void testIsJoker() {
+
+    }
+
+
+    //assert that a joker can be used in a meld (isValid() passes)
 
     //assert that a joker is counted as 30 points in meld.getScore()
 
+    //assert that Table.prettyString() shows the jokers properly
 
+    //discuss with everyone about the organization of a printed meld when the joker is included; 
+    //should it just read as a big "J" or something else?
+
+   /*
     //LATER...
 
-    /*  -> assert that jokers can be added to melds made from searchHand()
+        -> assert that jokers can be added to melds made from searchHand()
         -> assert that jokers can be added to melds made from searchTable()
         
         -> TBA: whether or not the AI know how to reuse jokers already on the table
