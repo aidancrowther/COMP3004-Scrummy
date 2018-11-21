1fb8f4ba2a13c94e3b3ca26d60396aee0a3e009d
A <ellisjglennie@gmail.com>
2018-11-20 21:35:00 -0500

    JokerTest: starting joker tests planning

---
+++ TileRummy/src/test/java/COMP3004/JokerTest.java
@@ -0,0 +1,34 @@
+package COMP3004;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import COMP3004.models.Tile;
+import COMP3004.models.Meld;
+import org.junit.Test;
+
+
+public class JokerTest {
+
+    //assert that Tile.isJoker() can identify which cards are jokers and which are not
+
+    //assert that a joker can be created and assigned different values
+    
+    //assert that a joker can be used in a meld
+
+    //assert that a joker is counted as 30 points in meld.getScore()
+
+
+    //LATER...
+
+    /*  -> assert that jokers can be added to melds made from searchHand()
+        -> assert that jokers can be added to melds made from searchTable()
+        
+        -> TBA: whether or not the AI know how to reuse jokers already on the table
+
+        -> assert that jokers will not just be appended to the fronts or ends of melds and will 
+            be used with some "cleverness"
+        -> assert that if the joker is the only card left in the hand, the ai plays it
+    */
+
+}
