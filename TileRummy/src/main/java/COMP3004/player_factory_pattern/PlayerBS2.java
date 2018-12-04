1e936be9bef6139d6cf9de3ca48a1559ea0b3249
Brittny Lapierre <brittny.lapierre@gmail.com>
2018-12-04 00:21:18 -0500

    reated scenario 2 player code

--- TileRummy/src/main/java/COMP3004/player_factory_pattern/PlayerBS2.java
+++ TileRummy/src/main/java/COMP3004/player_factory_pattern/PlayerBS2.java
@@ -4,19 +4,19 @@ import COMP3004.models.Player;
 import COMP3004.models.Tile;
 
 public class PlayerBS2 extends Player {
-    public PlayerAS2(){
-        this.hand.add(new Tile('G', 1));
-        this.hand.add(new Tile('G', 2));
-        this.hand.add(new Tile('B', 3));
-        this.hand.add(new Tile('G', 6));
-        this.hand.add(new Tile('G', 5));
-        this.hand.add(new Tile('B', 6));
-        this.hand.add(new Tile('G', 7));
-        this.hand.add(new Tile('G', 8));
-        this.hand.add(new Tile('B', 9));
-        this.hand.add(new Tile('G', 10));
-        this.hand.add(new Tile('O', 11));
-        this.hand.add(new Tile('O', 12));
-        this.hand.add(new Tile('O', 13));
+    public PlayerBS2(){
+        this.hand.add(new Tile('R', 1));
+        this.hand.add(new Tile('R', 2));
+        this.hand.add(new Tile('R', 3));
+        this.hand.add(new Tile('R', 6));
+        this.hand.add(new Tile('R', 5));
+        this.hand.add(new Tile('R', 6));
+        this.hand.add(new Tile('R', 7));
+        this.hand.add(new Tile('R', 8));
+        this.hand.add(new Tile('R', 9));
+        this.hand.add(new Tile('R', 10));
+        this.hand.add(new Tile('R', 11));
+        this.hand.add(new Tile('R', 12));
+        this.hand.add(new Tile('R', 13));
     }
 }
